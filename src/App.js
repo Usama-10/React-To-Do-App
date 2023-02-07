9a6143ac0976937f635bbf7a4a22c183b248fa11
Usama-10 <usamaisthiaq768@gmail.com>
2023-02-07 07:16:09 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const[todos, setTodos] = useState([]);
+const[text ,setText] = useState("");
+const[indexNum , SetindexNum] = useState();
+let add = () => {
+  if(indexNum > -1){
+    todos[indexNum] = text;
+    setTodos([...todos]);
+    SetindexNum(-1);
+  }
+  else{todos.push(text);
+  setTodos([...todos]);
+  }  
+  setText("");
+}
+let del = (i) =>{
+  todos.splice(i,1);
+  setTodos([...todos])
+}
+let edit = (i) => {
+  SetindexNum(i);
+  setText(todos[i])
+}
+return(
+<div className='App'>
+  <h1>Todo App</h1>
+  <input type="text" placeholder='Your Text' value={text} onChange={(e) => setText(e.target.value)} />
+  <button onClick={add}>Add</button>
+  {todos.map((x,i) => {
+    return(
+    <li key={i}>
+      {x}
+      <button onClick={() => del(i)}>Delete</button>
+      <button onClick={() => edit(i)}>Edit</button>
+
+    </li>
+    )
+  })}
+</div>
+)
 }
 
 export default App;
